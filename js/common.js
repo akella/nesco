9194aa36bd488b4805882619668307a4325342a8
akella <akella.a@gmail.com>
2014-05-01 00:29:36 +0300

    header

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-	console.log($('body').html());
+head.ready(function() {
+
+	$(window).scroll(function(){
+		t = $(this).scrollTop();
+		b = $('body');
+		if(t>100){
+			b.addClass('is-nottop');
+		}
+		else{
+			b.removeClass('is-nottop')
+		}
+	});
 });
