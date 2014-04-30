aeb8525adcc5b73313ea7469b9eda480cda45c60
akella <akella.a@gmail.com>
2014-05-01 02:18:37 +0300

    all galleries

--- js/common.js
+++ js/common.js
@@ -10,4 +10,14 @@ head.ready(function() {
 			b.removeClass('is-nottop')
 		}
 	});
+
+	$('.search__switcher a').click(function(event) {
+		$('.search__switcher a').removeClass('is-active');
+		$(this).addClass('is-active');
+		return false;
+	});
+	
+	$('.superslider__playpause').click(function(event) {
+		$('.superslider').cycle('stop');
+	});
 });
--- js/init.js
+++ js/init.js
@@ -1,1 +1,1 @@
-head.load("js/lib/jquery.js", "js/common.js");
+head.load("js/lib/jquery.js", "js/lib/cycle.js","js/lib/carousel.js", "js/common.js");
